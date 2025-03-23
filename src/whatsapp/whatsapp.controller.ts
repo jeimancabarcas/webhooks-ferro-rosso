7a8d3b1b8c2a643acac58d3a4a6df0c42244d4fb
Jeiman Cabarcas <jeimancabarcas@gmail.com>
2025-03-23 13:57:31 -0500

    fix verify token

--- src/whatsapp/whatsapp.controller.ts
+++ src/whatsapp/whatsapp.controller.ts
@@ -1,7 +1,22 @@
-import { Body, Controller, Headers, Post } from '@nestjs/common';
+import { Body, Controller, Get, Headers, Post, Query } from '@nestjs/common';
 
 @Controller('whatsapp/webhook')
 export class WhatsappController {
+  
+  private readonly VERIFY_TOKEN = 'MI_TOKEN_SEGURO';
+
+  @Get()
+  verifyWebhook(
+    @Query('hub.mode') mode: string,
+    @Query('hub.challenge') challenge: string,
+    @Query('hub.verify_token') token: string,
+  ): string {
+    if (mode === 'subscribe' && token === this.VERIFY_TOKEN) {
+      return challenge; // WhatsApp espera recibir este valor si todo está bien
+    }
+    return 'Token inválido'; // Si el token no coincide, WhatsApp no validará el webhook
+  }
+
   @Post()
   receiveMessage(@Body() body: any, @Headers() headers: any) {
     console.log('Mensaje recibido:', JSON.stringify(body, null, 2));
